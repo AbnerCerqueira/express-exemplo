e206c4d93e1bb44bd6717067b4864a752d8378ba
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-04-21 16:57:13 -0300

    Login

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const database = require('./database')
 const app = express()
 
 app.set("view engine", "ejs")
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(session({
     secret: 'me arrumem um emprego',
     resave: false,
@@ -13,7 +13,10 @@ app.use(session({
 
 // GET
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    const error = req.query.error
+    res.render("index.ejs", {
+        error
+    })
 })
 
 app.get("/cadastro", (req, res) => {
@@ -28,7 +31,16 @@ app.get("/sucess", (req, res) => {
 })
 
 app.get("/principal", (req, res) => {
-    res.render("principal.ejs")
+    if (!req.session.user) {
+        res.send("Você precisa fazer login para acessar essa página")
+        return
+    }
+    const user = {
+        username: req.session.user.username
+    }
+    res.render("principal.ejs", {
+        user
+    })
 })
 
 app.get("/atualiza", (req, res) => {
@@ -36,19 +48,39 @@ app.get("/atualiza", (req, res) => {
 })
 
 // POST
+// CREATE
 app.post("/cadastro", async (req, res) => {
     const data = req.body
-    if (!data.usernameInput || !data.passwordInput){
+    if (!data.usernameInput || !data.passwordInput) {
         res.redirect("/cadastro?error=empty")
         return
     }
-    if (data.passwordInput === data.passwordCheckInput){
+    if (data.passwordInput === data.passwordCheckInput) {
         await database.setData(data)
         res.redirect("/sucess")
         return
     }
     res.redirect("/cadastro?error=error")
 })
+
+// READ
+app.post("/login", async (req, res) => {
+    const data = req.body
+    const rows = await database.getData(data)
+    if (rows.length > 0) {
+        req.session.user = {
+            id: rows[0].id,
+            username: rows[0].username
+        }
+        res.redirect("/principal")
+        return
+    }
+    res.redirect("/?error=error")
+})
+
+// UPDATE
+
+// DELETE
 
 //
 app.use(express.static('static'))
