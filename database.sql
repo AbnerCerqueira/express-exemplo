fe1dee57f40eceeae5d775b3cde88bba15a28f52
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-05-10 08:07:27 -0300

    style: database

--- database.sql
+++ database.sql
@@ -1,9 +1,10 @@
 -- Active: 1715288619464@@127.0.0.1@3306@fatec
+CREATE DATABASE fatec;
+
+USE fatec;
+
 CREATE TABLE User (
     id int PRIMARY KEY AUTO_INCREMENT,
     username varchar(50) NOT NULL,
     password varchar(50) NOT NULL
 );
-
-
-select * from `User`;
